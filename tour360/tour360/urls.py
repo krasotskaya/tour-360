f59e77613fea21263369a73234a50ffea0718c38
Asideron <45494390+Asideron@users.noreply.github.com>
2020-12-11 23:18:34 +0300

    Initial commit

---
+++ tour360/tour360/urls.py
@@ -0,0 +1,29 @@
+"""tour360 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+APPS_URL_DIR = 'tour360.apps.'
+
+
+def app_urls(urls_file):
+    return APPS_URL_DIR + urls_file
+
+
+urlpatterns = [
+    path('', include(app_urls('mainpage.urls'))),
+    path('admin/', admin.site.urls),
+]
